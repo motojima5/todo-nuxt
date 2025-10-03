bb757b702bb729f6bd3ddea80e76f0836d2d9d50
Motojima <r.motojima5@gmail.com>
2025-10-03 15:55:44 +0900

    ログイン機能の実装

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>TODO</h1>
+    <TheLoggedInSection />
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
